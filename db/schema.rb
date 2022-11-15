99c7b3d33f32416ee7956a8ebb94475f54291852
Cheyenne Fontes <cheyennefontes@hotmail.com>
2022-11-15 11:26:48 -0300

    rails new

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2022_11_15_013230) do
+ActiveRecord::Schema[7.0].define(version: 2022_11_15_124614) do
   create_table "tasks", force: :cascade do |t|
     t.string "title"
     t.string "details"
